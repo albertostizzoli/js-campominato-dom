f6b1434e7d62b28d45f863d4816a4e49337b1512
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-19 14:42:35 +0200

    Modifications

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
+// Creo una variabile  per il bottone
+const btn = document.querySelector('button');
 
-// creo una funzione che generi una nuova partita
-function createNewGame(){
-    let cellsNumber;
-    let cellsRow;
-
-    let arrayBombs = [];
-
-    if(difficulty.value == 'Facile'){
-        cellsNumber = 100;
-        cellsRow = 10;
+btn.addEventListener('click', function(){
+    // numero dei quadratini
+    const numSquare = 100;
+    // Griglia di gioco
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+    // Creo un ciclo per stampare i quadratini
+    for(let i = 0; i < numSquare; i++){
+        let square = drawSquare(i, numSquare);
+        // Appendo i quadratini alla griglia
+        grid.append(square);
     }
-    else if(difficulty.value == 'Media'){
-        cellsNumber = 81;
-        cellsRow = 9;
-    }
-    else{
-        cellsNumber = 49;
-        cellsRow = 7;
-    }
+});
 
-    arrayBombs = createBombsArray(1, cellsNumber);
-    //stampo in console log l'array in ordine crescente
-    console.log(arrayBombs.sort(function(a, b){return a-b}));
+// Creo una funzione che disegni i quadratini
+ function drawSquare(squareIndex,numSquare){
+    // Creo una variabile che divida la griglia in 100 quadratini
+    const squareWidth = Math.sqrt(numSquare);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    // Altezza e Larghezza dei quadratini
+    square.style.width = `calc(100% / ${squareWidth})`;
+    square.style.width = square.style.width;
+    square.innerHTML = squareIndex + 1;
+    // Funzione che permetta all'utente di cambiare colore del quadratino cliccandoci sopra
+    square.addEventListener('click', function(){
+        square.classList.add('active');
+        square.style.color = 'black';
+        console.log(square.textContent);
+    });
+      return square;
+ }
 
-    generateGrid(arrayBombs, cellsNumber, cellsRow);
-}
