ce4635686dab859a52ca1cb7e5e872baddc0fe38
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-19 14:37:20 +0200

    Added Function for Create a New Game

--- js/script.js
+++ js/script.js
@@ -34,4 +34,29 @@ btn.addEventListener('click', function(){
       return square;
  }
 
+// creo una funzione che generi una nuova partita
+function createNewGame(){
+    let cellsNumber;
+    let cellsRow;
 
+    let arrayBombs = [];
+
+    if(difficulty.value == 'Facile'){
+        cellsNumber = 100;
+        cellsRow = 10;
+    }
+    else if(difficulty.value == 'Media'){
+        cellsNumber = 81;
+        cellsRow = 9;
+    }
+    else{
+        cellsNumber = 49;
+        cellsRow = 7;
+    }
+
+    arrayBombs = createBombsArray(1, cellsNumber);
+    //stampo in console log l'array in ordine crescente
+    console.log(arrayBombs.sort(function(a, b){return a-b}));
+
+    generateGrid(arrayBombs, cellsNumber, cellsRow);
+}
